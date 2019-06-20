e857dec5dc9d0495edffcf970f0dee266b14bd5d
Yosef Lehrer <yolehrer@gmail.com>
2019-06-20 04:20:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+   empty_array = []
+   line(katz_deli).each_with_index do |name, index|
+     empty_array.push "#{index}. #{name}"
+   end
+  end
+  empty_array
+end
+
