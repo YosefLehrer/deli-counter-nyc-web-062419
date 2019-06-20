77ab705f46a708eb2d3d2894cdac1bd021da7385
Yosef Lehrer <yolehrer@gmail.com>
2019-06-20 04:32:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,9 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
 
+def now_serving(array)
+  if array.size == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
